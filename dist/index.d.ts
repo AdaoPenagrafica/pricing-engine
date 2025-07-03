2e285de5807375dc954dc2867d294aaac44b42aa
AdaoPenagrafica <adao@penagrafica.pt>
2025-07-03 15:20:08 +0100

    new

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,7 @@
+export { evaluateRules } from './ruleEngine';
+export { default as bookRules } from '../rules/book.rules.json';
+export { default as autocRules } from '../rules/autoc.rules.json';
+import { specialPrice } from './helpers/autoc.helper';
 export declare const ruleMap: {
     book: {
         _note: string;
@@ -26,7 +30,6 @@ export declare const ruleMap: {
         };
     }[];
 };
-import { specialPrice } from './helpers/autoc.helper';
 export declare const helperMap: {
     book: {};
     autoc: {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+export { evaluateRules } from './ruleEngine';
+export { default as bookRules } from '../rules/book.rules.json';
+export { default as autocRules } from '../rules/autoc.rules.json';
+
 import bookRules from '../rules/book.rules.json';
 import autocRules from '../rules/autoc.rules.json';
+import { specialPrice } from './helpers/autoc.helper';
 
 export const ruleMap = {
   book: bookRules,
   autoc: autocRules
 };
-
-import { specialPrice } from './helpers/autoc.helper';
 
 export const helperMap = {
   book: {},
